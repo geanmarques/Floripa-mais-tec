8babcdcf944ae6073e370aeffae7dd8af3df39d0
Gean Marques Veloso <88169917+geanmarques@users.noreply.github.com>
2023-02-24 07:30:59 -0300

    alterei a função

--- MS1S03/funcao-par-impar.js
+++ MS1S03/funcao-par-impar.js
@@ -1,9 +1,11 @@
-function parImpar(numero) {
+// Transforma um número decimal em inteiro e diz se ele é par ou impar
+
+function parImpar(num) {
+    let numero = parseInt(num)
     if (numero % 2 == 0) {
         console.log('O número '+ numero + ' é par');
     }else{
         console.log('O número '+ numero + ' é impar');
     }
-
 }
-parImpar(1.2);
+parImpar(2003.99995);
