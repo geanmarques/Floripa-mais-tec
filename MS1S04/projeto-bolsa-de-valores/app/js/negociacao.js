208b2818b5bc219dc3f947e1e2e185de35d953ff
Gean Marques Veloso <88169917+geanmarques@users.noreply.github.com>
2023-03-06 17:17:36 -0300

    criação da classe

---
+++ MS1S04/projeto-bolsa-de-valores/app/js/negociacao.js
@@ -0,0 +1,8 @@
+class negociacao {
+    constructor (data, quantidade, valor){
+        this.data = new Date();
+        this.quantidade = new Number();
+        this.valor = new Number();
+    }
+
+}
